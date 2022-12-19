349e8975ba73641fd0be4f8d48f9fed79e5148f8
kalyani chagala <89970618+kalyanichagala@users.noreply.github.com>
2022-12-20 03:47:58 +0530

    Update haiglastest.py

--- haiglastest.py
+++ haiglastest.py
@@ -99,21 +99,21 @@ y_train
 
 
 from sklearn.model_selection import RandomizedSearchCV
-# Number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start = 200, stop = 300, num = 10)]
-# Number of features to consider at every split
-# Maximum number of levels in tree
-max_depth = [int(x) for x in np.linspace(10, 55, num = 11)]
-max_depth.append(None)
-# Minimum number of samples required to split a node
-min_samples_split = [2, 5, 10]
-# Minimum number of samples required at each leaf node
-min_samples_leaf = [1, 2, 4]
-# Method of selecting samples for training each tree
-bootstrap = [True, False]
-# Create the random grid
-random_grid = {'n_estimators': n_estimators,
-               'max_depth': max_depth}
+# # Number of trees in random forest
+# n_estimators = [int(x) for x in np.linspace(start = 200, stop = 300, num = 10)]
+# # Number of features to consider at every split
+# # Maximum number of levels in tree
+# max_depth = [int(x) for x in np.linspace(10, 55, num = 11)]
+# max_depth.append(None)
+# # Minimum number of samples required to split a node
+# min_samples_split = [2, 5, 10]
+# # Minimum number of samples required at each leaf node
+# min_samples_leaf = [1, 2, 4]
+# # Method of selecting samples for training each tree
+# bootstrap = [True, False]
+# # Create the random grid
+# random_grid = {'n_estimators': n_estimators,
+#                'max_depth': max_depth}
 
 
 # In[55]:
@@ -129,9 +129,9 @@ from sklearn.ensemble import RandomForestClassifier
 rf = RandomForestClassifier()
 # Random search of parameters, using 3 fold cross validation, 
 # search across 100 different combinations, and use all available cores
-rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 10,cv=2, verbose=2, random_state=42, n_jobs = -1)
+#rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 10,cv=2, verbose=2, random_state=42, n_jobs = -1)
 # Fit the random search model
-rf_random.fit(X_train, y_train)
+rf.fit(X_train, y_train)
 
 
 # In[40]:
@@ -179,7 +179,7 @@ X_test=X_train
 # In[47]:
 
 
-y_test=rf_random.predict(X_test)
+y_test=rf.predict(X_test)
 
 
 # In[48]:
